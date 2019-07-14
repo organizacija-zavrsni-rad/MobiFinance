94f47654d1becfb8fa71fa685169cd16ec6dc39f
Ivan Sršen <ivan.srsen@enterwell.net>
2019-07-14 10:14:52 +0200

    Main method added

---
+++ MobiFinance/MobiFinance/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MobiFinance
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+        }
+    }
+}
